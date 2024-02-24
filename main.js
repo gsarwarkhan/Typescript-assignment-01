7d4b0b73ac3ec5af69d25d1d53b44d06254fba32
Ghulam Sarwar Khan <160528570+gsarwarkhan@users.noreply.github.com>
2024-02-25 02:19:32 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Assalam Waalaiku');
+console.log('My name is Ghulam Sarwar Khan');
+console.log('this is my first time doing coding');
+console.log('I am attending wonderful classes at Governor House');
+console.log('I Wish to learn Generative AI to develop my skills');
+console.log('with the knowledge i want to contribute society');
